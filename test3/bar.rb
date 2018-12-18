3e89242cd3714b7a795d821bb169debdad9a15b0
Matthew Lanigan <matthew.lanigan@lendinghome.com>
2018-12-18 09:36:27 -0500

    Test #3: Foo < Bar - pass

---
+++ test3/bar.rb
@@ -0,0 +1,12 @@
+module Test3
+  class Bar
+    # @!macro [attach] field
+    #   @!attribute [r] $1
+    #   @return [String, nil] the field `$1`
+    def self.field(name)
+      define_method(name) do
+        @fields[name]
+      end
+    end
+  end
+end
---
+++ test3/foo.rb
@@ -0,0 +1,6 @@
+module Test3
+  class Foo < Bar
+    # Test field
+    field :test
+  end
+end
